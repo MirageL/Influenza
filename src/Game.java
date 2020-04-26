c5584a0dea384d42375335532ed18b9fbdb773cb
Mirage Leung <m_leung08@hotmail.com>
2020-04-26 16:30:41 -0700

    Push still not working via git bash, changed color of render

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,11 @@
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
 import java.awt.Canvas;
+import java.awt.Color;
 
 public class Game extends Canvas implements Runnable{
     public static final int WIDTH = 270;
@@ -41,32 +44,48 @@ public class Game extends Canvas implements Runnable{
     public void run(){
         long lastTime = System.nanoTime();
         long timer = System.currentTimeMillis();
-        double delta = 0;
+        double delta = 0.0;
         double ns = 1000000000.0/60.0;
         int frames = 0;
         int ticks = 0;
         
         while(running){
             long now = System.nanoTime();
+            //System.out.println("Now: " + now);
+            //System.out.println("lastTime: " + lastTime);
             delta += (now-lastTime)/ns;
+            //System.out.println("delta: " + delta);
             lastTime = now;
             while(delta>=1){
                 tick();
                 ticks++;
-                delta++;
+                delta--;
             }
             render();
             frames++;
             if(System.currentTimeMillis()-timer>1000) {
                 timer += 1000;
-                System.out.println(frames + " Frames per second" + ticks + " Updates per second");
+                System.out.println(frames + " Frames per second and " + ticks + " Updates per second");
                 frames = 0;
                 ticks = 0;
             }
         }
+        stop();
     }
 
-    public void render(){}
+    public void render(){
+        BufferStrategy bs = getBufferStrategy();
+
+        if(bs==null){
+            createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+        g.setColor(new Color(124,28,97));
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.dispose();
+        bs.show();
+    }
 
     public void tick(){}
 
